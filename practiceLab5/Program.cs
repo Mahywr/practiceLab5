e05cb71840e0a7f81a5ad38a4efb57e0d36d7591
802997 <802997@FENNER-177-24>
2023-11-01 10:37:40 +0000

    Add project files.

---
+++ practiceLab5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Square
+{
+    public static void Main()
+    {
+        Console.Write("Enter a number: "); // Prompt the user for input
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            int squared = number * number;
+            Console.WriteLine("The square of {0} is {1}", number, squared);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+}
